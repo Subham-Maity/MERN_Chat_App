17ba97c778bd42f2fb8e6a8b6603167ca825453b
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-04-15 21:31:49 +0530

    Remove router.asPath === "/ChatPage because we use dynamic rendering

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,18 +1,8 @@
-// pages/_app.tsx
-
 import { ChakraProvider } from "@chakra-ui/react";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { useRouter } from "next/router";
-import ChatPage from "./ChatPage";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  if (router.asPath === "/chats" || router.asPath === "/ChatPage") {
-    return <ChatPage />;
-  }
-
   return (
     <ChakraProvider>
       <Component {...pageProps} />
