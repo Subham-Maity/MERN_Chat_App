6200c35c552f5bba3d90ff3c9139cc6f4914c98b
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-04-15 17:54:28 +0530

    Chakra Ui Setup & Use useRouter from next/router
    Use router.asPath for redirect the path
    
    in react (exact and Path and react-router dom)

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// pages/_app.tsx
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { ChakraProvider } from "@chakra-ui/react";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import ChatPage from "./ChatPage";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  if (router.asPath === "/chats" || router.asPath === "/ChatPage") {
+    return <ChatPage />;
+  }
+
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
+
+export default MyApp;
