e22102ce09d33a6229f1b7bcc87afe063118af4f
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-04-15 22:49:12 +0530

    Display name over frontend part usind Usestate

--- frontend/src/pages/ChatPage.tsx
+++ frontend/src/pages/ChatPage.tsx
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const ChatPage = () => {
+  const [chats, setChats] = React.useState([]);
   const fetchChats = async () => {
     // const data = await axios.get("http://localhost:5000/api/chats");
     const { data } = await axios.get("http://localhost:5000/api/chats"); //destructure data from response
-    console.log(data);
+    setChats(data);
   };
 
   React.useEffect(() => {
     fetchChats();
   }, []);
 
-  return <div>ChatPage</div>;
+  return (
+    <div>
+      {chats.map((chats) => (
+        <div key={chats._id}>{chats.chatName}</div>
+      ))}
+    </div>
+  );
 };
 
 export default ChatPage;
