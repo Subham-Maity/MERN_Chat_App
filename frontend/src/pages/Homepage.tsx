a5bbc4f7aa20d113ae9a3a8bc35dbba2c84d98b3
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-04-16 23:35:23 +0530

    Adding Tailwind Class and background change

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,41 +1,52 @@
 import React from "react";
 import {
-  Box,
   Container,
+  Box,
   Tab,
   TabList,
   TabPanel,
   TabPanels,
   Tabs,
-  Text,
 } from "@chakra-ui/react";
+
 const Homepage = () => {
   return (
-    <Container maxW="xl" centerContent>
+    <Container maxW="xl" centerContent mt="8">
       <Box
-        d="flex"
-        justifyContent="center"
-        p={3}
-        bg="white"
-        w="100%"
-        m="40px 0 15px 0"
-        borderRadius="lg"
-        borderWidth="1px"
+        bg="rgba(31, 41, 55, 0.8)"
+        rounded="lg"
+        border="1px"
+        borderColor="gray.700"
+        p="6"
+        mb="4"
+        w="full"
       >
-        <Text fontSize="4xl" fontFamily="Work sans">
-          Octo Meet
-        </Text>
+        <h1 className="text-white text-4xl font-semibold">Octo Meet</h1>
       </Box>
-      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+      <Box
+        bg="rgba(31, 41, 55, 0.4)"
+        rounded="lg"
+        border="1px"
+        borderColor="gray.700"
+        p="6"
+        mb="4"
+        w="full"
+      >
         <Tabs isFitted variant="soft-rounded">
-          <TabList mb="1em">
-            <Tab>Login</Tab>
-            <Tab>Sign Up</Tab>
+          <TabList mb="4">
+            <Tab
+              className="text-white rounded-lg border border-gray-700 p-2 m-2 w-full text-center transition-colors duration-300 hover:bg-gray-700 hover:border-gray-800 hover:text-gray-200"
+              _focus={{ outline: "none", boxShadow: "outline" }}
+            >
+              Login
+            </Tab>
+            <Tab
+              className="text-white rounded-lg border border-gray-700 p-2 m-2 w-full text-center transition-colors duration-300 hover:bg-gray-700 hover:border-gray-800 hover:text-gray-200"
+              _focus={{ outline: "none", boxShadow: "outline" }}
+            >
+              Sign Up
+            </Tab>
           </TabList>
-          <TabPanels>
-            <TabPanel>{/* <Login /> */}</TabPanel>
-            <TabPanel>{/* <Signup /> */}</TabPanel>
-          </TabPanels>
         </Tabs>
       </Box>
     </Container>
