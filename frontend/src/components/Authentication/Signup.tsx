6d780ea48ca44c02c30ea16a60d58312183d28c1
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-04-17 13:57:34 +0530

    Password Hide & Show using useState(show,hide)

--- frontend/src/components/Authentication/Signup.tsx
+++ frontend/src/components/Authentication/Signup.tsx
@@ -1,15 +1,16 @@
 import {VStack} from "@chakra-ui/layout";
-import {FormControl, FormLabel} from "@chakra-ui/react";
+import {FormControl, FormLabel, InputGroup, InputRightElement} from "@chakra-ui/react";
 import React, {useState} from "react";
 import styles from "./Signup.module.css";
 
 const Signup = () => {
+    const [show, setShow] = useState(false); //data type as boolean
     const [name, setName] = useState<string>(""); //data type as string
     const [email, setEmail] = useState();
     const [confirmpassword, setConfirmpassword] = useState();
     const [password, setPassword] = useState();
     const [pic, setPic] = useState();
-
+    const handleClick = () => setShow(!show);
     return (
         <VStack spacing='5px' color='gray.700'>
             <FormControl className={styles.container} id='first-name' isRequired>
@@ -21,6 +22,25 @@ const Signup = () => {
                     placeholder='Hey there, what’s your name?'
                     onChange={(e) => setName(e.target.value)}
                 />
+            </FormControl>
+            <FormControl className={styles.container} id='password' isRequired>
+
+                <FormLabel className="label" color='gray.200'>Password </FormLabel>
+                <InputGroup>
+                    <input
+                        type={show ? "text" : "password"}
+                        className={`${styles.input} input`}
+                        placeholder='Enter your password'
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                    <InputRightElement width="4.5rem">
+                        <button className="text-white" onClick={handleClick}>
+                            {show ? "Hide" : "Show"}
+                        </button>
+                    </InputRightElement>
+
+                </InputGroup>
+
             </FormControl>
         </VStack>
     );
