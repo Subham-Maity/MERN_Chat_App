bc5d8cafeed3a35ef80a0860c46763aa032bf3c3
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-04-17 13:31:56 +0530

    ClassName + Name field create (custom css + tailwind + chakra ui

--- frontend/src/components/Authentication/Signup.tsx
+++ frontend/src/components/Authentication/Signup.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import {VStack} from "@chakra-ui/layout";
+import {FormControl, FormLabel} from "@chakra-ui/react";
+import React, {useState} from "react";
+import styles from "./Signup.module.css";
 
 const Signup = () => {
-  return <div>Signup</div>;
+    const [name, setName] = useState<string>(""); //data type as string
+    const [email, setEmail] = useState();
+    const [confirmpassword, setConfirmpassword] = useState();
+    const [password, setPassword] = useState();
+    const [pic, setPic] = useState();
+
+    return (
+        <VStack spacing='5px' color='gray.700'>
+            <FormControl className={styles.container} id='first-name' isRequired>
+
+                <FormLabel className="label" color='gray.200'>Name </FormLabel>
+                <input
+                    type="text"
+                    className={`${styles.input} input`}
+                    placeholder='Hey there, what’s your name?'
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </FormControl>
+        </VStack>
+    );
 };
 
 export default Signup;
