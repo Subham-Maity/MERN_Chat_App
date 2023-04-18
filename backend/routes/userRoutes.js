73706ac814121510db9078aa54049468eb7d4320
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-04-18 14:06:29 +0530

    Server.js fixed , userRoutes Setup

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+
+// router.route("/").post(registerUser);
+// router.post("/login", authUser);
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,18 @@
 const express = require("express");
-const chats = require("./data/data");
-const cors = require("cors");
-// Load the dotenv module before calling connectDB
 const dotenv = require("dotenv");
-dotenv.config();
-const app = express();
+const { chats } = require("./data/data");
 const connectDB = require("./config/db");
+const cors = require("cors");
+dotenv.config();
+connectDB();
+const app = express();
 
 app.use(cors());
-connectDB();
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
-
-app.get("/api/chats", (req, res) => {
-  res.send(chats);
-});
-
-app.get("/api/chats/:id", (req, res) => {
-  //console.log(req);
-  // console.log(req.params.id);
-  const chat = chats.find((c) => c._id === req.params.id);
-  res.send(chat);
-});
-
+app.use("/api/user", userRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
