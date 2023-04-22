e420ec077e8ad7aa915bede5e28246fb7c8da80f
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-04-23 00:50:05 +0530

    Error Handling + notFound

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 const cors = require("cors");
 dotenv.config();
 connectDB();
@@ -17,6 +18,11 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 app.use("/api/user", userRoutes);
+
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
